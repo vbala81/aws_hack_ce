bda788518e927adee0b9ee3645c4b29b55fa797c
vbala81 <B.E.VISWANATHAN@ACCENTURE.COM>
2022-09-26 17:05:53 -0400

    Updated led

--- led.py
+++ led.py
@@ -3,7 +3,9 @@ import time
 LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
-GPIO.output(LED_PIN, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(LED_PIN, GPIO.LOW)
+while True:
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_PIN, GPIO.LOW)
+    time.sleep(1)
 GPIO.cleanup()
