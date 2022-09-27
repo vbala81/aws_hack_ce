446a469034cfbd7847efdb165c01a64c92827971
vbala81 <B.E.VISWANATHAN@ACCENTURE.COM>
2022-09-27 14:04:27 -0400

    Updated led

--- circular_economy.py
+++ circular_economy.py
@@ -30,8 +30,8 @@ def detect_motion():
     """
     try:
         while True:
-            print(GPIO.input(pir))
-            if (GPIO.input(pir) is True):  # If PIR pin goes high, motion is detected
+            gpio_input = GPIO.input(pir)
+            if (gpio_input == 1):  # If PIR pin goes high, motion is detected
                 print("Motion Detected!")
                 GPIO.output(led, True)  # Turn on LED
                 time.sleep(4)  # Keep LED on for 4 seconds
