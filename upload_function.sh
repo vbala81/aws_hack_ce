406c8ff81d2d8ed10c2744996d20e033e149ab57
Nikhil Mathur <mathurn@calsaws.org>
2022-09-28 09:12:20 -0400

    Updated gitignore to include pymysql folders as well as updated upload_function script to include that as well

--- upload_function.sh
+++ upload_function.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-zip lambda_function.zip lambda_function.py
+zip -r lambda_function.zip lambda_function.py rds_config.py pymysql/ PyMySQL-1.0.2.dist-info/
 aws lambda update-function-code --function-name CircularEconomyLambda --zip-file fileb://lambda_function.zip
 aws s3 cp lambda_function.zip s3://hackathon-ce-bucket/
 rm lambda_function.zip
